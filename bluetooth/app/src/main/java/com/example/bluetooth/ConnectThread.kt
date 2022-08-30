90b9f43e90fda7d6628fce15eadd0d0f986213d0
Ru7tam <105158227+Ru7tam@users.noreply.github.com>
2022-08-30 13:29:22 +0300

    bluetooth
    bluetooth

---
+++ bluetooth/app/src/main/java/com/example/bluetooth/ConnectThread.kt
@@ -0,0 +1,37 @@
+package com.example.bluetooth
+
+import android.annotation.SuppressLint
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothSocket
+import java.io.IOException
+import java.util.*
+
+@SuppressLint("MissingPermission")
+class ConnectThread(private val device: BluetoothDevice) : Thread() {
+    val uuid = "00001101-0000-1000-8000-00805F9B34FB"
+    var mSocket: BluetoothSocket? = null
+
+    init {
+        try {
+            mSocket = device.createRfcommSocketToServiceRecord(UUID.fromString(uuid))
+        } catch (i: IOException) {
+
+        }
+    }
+
+    override fun run() {
+        try {
+            mSocket?.connect()
+        } catch (i: IOException) {
+            closeConnection()
+        }
+    }
+
+    fun closeConnection() {
+        try {
+            mSocket?.close()
+        } catch (i: IOException) {
+
+        }
+    }
+}
